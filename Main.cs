de5c3b7530494ba65131e4d7206733c06b14c953
MrFlooky <42610166+MrFlooky@users.noreply.github.com>
2024-08-25 00:34:37 +0300

    upd - better regex for domain & added doc for main method

--- Main.cs
+++ Main.cs
@@ -4,6 +4,21 @@ using System.Text.RegularExpressions;
 namespace EmailDnsChecking;
 
 public static partial class EmailDnsChecker {
+	/// <summary>
+	/// Asynchronously checks the DNS records for the email domain to determine its validity.
+	/// </summary>
+	/// <param name="email">The email address to validate.</param>
+	/// <param name="timeout">Optional. The DNS query timeout in seconds. Default is 5 seconds.</param>
+	/// <returns>
+	/// A task that represents the asynchronous operation. The task result contains a <see cref="DnsResult"/> object
+	/// which indicates whether the email domain is valid and provides a status message.
+	/// </returns>
+	/// <remarks>
+	/// The method first validates the email format and domain. It then performs a DNS MX record query
+	/// to check if the domain has valid mail exchange (MX) records. The result indicates if the domain is valid or if there was an error.
+	/// </remarks>
+	/// <exception cref="DnsResponseException">Thrown when the DNS query fails.</exception>
+	/// <exception cref="Exception">Thrown when an unexpected error occurs.</exception>
 	public async static Task<DnsResult> CheckEmailAsync(string email, int timeout = 5) {
 		if (string.IsNullOrEmpty(email) || !email.Contains('@'))
 			return new DnsResult("Invalid email address.", 1);
@@ -34,7 +49,7 @@ public static partial class EmailDnsChecker {
 			Timeout = TimeSpan.FromSeconds(timeout)
 		});
 
-	[GeneratedRegex(@"^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$")]
+	[GeneratedRegex(@"^(?:xn--[a-zA-Z0-9-]+[a-zA-Z0-9]|(?!.{64,})[A-Za-z0-9]+(?:(?!.*-{2,})[A-Za-z0-9-]+[A-Za-z0-9])?)(?:\.xn--[a-zA-Z0-9-]+[a-zA-Z0-9]|\.[A-Za-z]{2,18})*$")]
 	private static partial Regex DomainRegex();
 }
 
